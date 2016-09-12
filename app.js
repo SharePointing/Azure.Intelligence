9c9030d24f5a4045be6d502b654003b504e76d85
Steven Follis <steven_follis@live.com>
2016-09-12 16:51:21 -0400

    Adjusted name

--- app.js
+++ app.js
@@ -68,5 +68,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.PORT || 3000);
 
-module.exports = app;
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
